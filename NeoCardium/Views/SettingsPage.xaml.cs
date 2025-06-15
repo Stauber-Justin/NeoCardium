d53f2c3e89dda4d4a070178611ebf1c051a05555
Justin <124533023+Stauber-Justin@users.noreply.github.com>
2025-06-16 00:31:44 +0200

    Add theme and account settings

--- NeoCardium/Views/SettingsPage.xaml.cs
+++ NeoCardium/Views/SettingsPage.xaml.cs
@@ -1,17 +1,7 @@
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
+using NeoCardium.ViewModels;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -23,9 +13,17 @@ namespace NeoCardium.Views
     /// </summary>
     public sealed partial class SettingsPage : Page
     {
+        public SettingsPageViewModel ViewModel { get; } = new SettingsPageViewModel();
+
         public SettingsPage()
         {
             this.InitializeComponent();
+            DataContext = ViewModel;
+
+            if (Enum.TryParse<ElementTheme>(ViewModel.SelectedTheme, out var theme) && App._mainWindow?.Content is FrameworkElement root)
+            {
+                root.RequestedTheme = theme;
+            }
         }
     }
 }
