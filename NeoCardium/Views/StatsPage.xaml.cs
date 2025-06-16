04169f0a57fb409a6be4085a73b8020fcd4b5525
Justin <124533023+Stauber-Justin@users.noreply.github.com>
2025-06-16 10:21:40 +0200

    Remove x:DataType from pages

--- NeoCardium/Views/StatsPage.xaml.cs
+++ NeoCardium/Views/StatsPage.xaml.cs
@@ -1,12 +1,16 @@
 using Microsoft.UI.Xaml.Controls;
+using NeoCardium.ViewModels;
 
 namespace NeoCardium.Views
 {
     public sealed partial class StatsPage : Page
     {
+        public StatsPageViewModel ViewModel { get; } = new StatsPageViewModel();
+
         public StatsPage()
         {
             this.InitializeComponent();
+            DataContext = ViewModel;
         }
     }
 }
